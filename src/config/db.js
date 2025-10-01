47ab62ec2091892ff27bd945e4963542fabedc07
[200~kubra-Islami <kubra.islami.00@gmail.com>
2025-10-01 11:27:43 +0100

    the initTables.js file was added to create tables

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ import {Pool} from "pg";
 
 const pool = new Pool(
     process.env.DATABASE_URL
-        ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+        ? { connectionString: process.env.DATABASE_URL,  ssl: { rejectUnauthorized: false } }
         : {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
@@ -14,17 +14,5 @@ const pool = new Pool(
             database: process.env.DB_NAME,
         }
 );
-// const pool = new Pool({
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASSWORD,
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         database: process.env.DB_NAME,
-//     }
-// );
-//
-// const pool = new Pool({
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: { rejectUnauthorized: false }
-// });
+
 export default pool;
